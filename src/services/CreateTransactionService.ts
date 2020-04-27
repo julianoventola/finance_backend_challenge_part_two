819369841c1c918e2bbda8e8fc76e0cc848038ad
JulianoV <ventolajuliano@gmail.com>
2020-04-27 13:32:17 -0300

    feat: Create Transaction, Create Category, Delete Transaction, Validations and Import transactions with categories from CSV file

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (total < value && type === 'outcome') {
+      throw new AppError('Insufficient balance value to outcome', 400);
+    }
+
+    let transactioCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!transactioCategory) {
+      transactioCategory = categoryRepository.create({ title: category });
+
+      await categoryRepository.save(transactioCategory);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: transactioCategory,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
